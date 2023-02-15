dd9009c2391c9c5188cc646536f444b673864a47
Vinit Gupta <vinitman35@gmail.com>
2023-02-15 12:03:02 +0530

    add smart contract and tests

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,4 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ETHDaddy {}
+contract ETHDaddy is ERC721{
+    uint256 public maxSupply;
+    address public owner;
+
+    struct Domain{
+        string name;
+        uint256 cost;
+        bool isOwned;
+    }
+
+    mapping(uint256 => Domain) domains;
+
+    constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol){
+        //* your code here
+        owner = msg.sender;
+    }
+
+    function list(string memory _name, uint256 _cost) public {
+        require(msg.sender == owner);
+        maxSupply++;
+        domains[maxSupply] = Domain(_name, _cost, false);
+    }
+
+    function getDomain(uint256 _id) public view returns (Domain memory){
+        return domains[_id];
+    }
+
+    function mint(uint256 _id) public payable {
+        _safeMint(msg.sender, _id);
+    }
+
+    function getBalance() public view returns (uint256){
+        return address(this).balance;
+    }
+    
+}
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,48 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
+  let ethDaddy, deployer, owner1; 
+  const NAME = 'ETH Daddy';
+  const SYMBOL = 'ETHD';
+  beforeEach(async() => {
+    [deployer, owner1] = await ethers.getSigners();
+    
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy');
+    ethDaddy = await ETHDaddy.deploy('ETH Daddy', 'ETHD');
 
+    // List Domain
+    const transaction = await ethDaddy.connect(deployer).list('john.eth', tokens(10));
+    await transaction.wait();
+  })
+  
+  describe('Deployment', () => {
+    it("has a name", async() => {
+      const result = await ethDaddy.name();
+      expect(result).to.equal(NAME);
+    })
+    it("has a symbol", async() => {
+      const result = await ethDaddy.symbol();
+      expect(result).to.equal(SYMBOL);
+    })
+    it('Sets the owner', async () => {
+      const result = await ethDaddy.owner();
+      expect(result).to.equal(deployer.address)
+    })
+    it('Returns the max Supply', async ()=>{
+      const result = await ethDaddy.maxSupply();
+      expect(result).to.equal(1);
+    })
+  })
+
+  describe('Domain', () =>{
+    it('Returns domain attributes', async () =>{
+      const domain = await ethDaddy.getDomain(1);
+      expect(domain.name).to.be.equal('john.eth');
+    })
+  })
 })
